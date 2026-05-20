6bf9de76373a618538735a7ed7c38b434c7f7d43
LucasFeitosa77 <lucassgabrieel2020@gmail.com>
2026-05-20 08:56:06 -0300

    feat: adicionar modelos de Pedido e ItensPedido, e configurar Alembic para migrações

--- models.py
+++ models.py
@@ -32,28 +32,7 @@ class Usuario(Base):
         self.ativo = ativo
         self.admin = admin
 
-# Pedido
-class Pedido(Base):
-    __table_name__ = 'pedidos'
-
-    # Escolha de somente três informações para o campo "status"
-    STATUS_PEDIDOS = (
-        ("PENDENTE", "PENDENTE"),
-        ("FINALIZADO", "FINALIZADO"),
-        ("CANCELADO", "CANCELADO")
-    )
-
-    id = Column("id", Integer, primary_key=True, autoincrement=True)
-    status = Column("status", ChoiceType(choices=STATUS_PEDIDOS), nullable=False, default="PENDENTE")
-    id_usuario = Column("id_usuario", ForeignKey("usuarios.id"), nullable=False)
-    preco = Column("preco", Float, nullable=False)
-#    itens =
-
-    def __init__(self, usuario, status="PENDENTE", preco=0):
-        self.usuario = usuario
-        self.status = status
-        self.preco = preco
-
+# Sabores
 class Sabores(Base):
     __table_name__ = 'sabores'
 
@@ -66,10 +45,10 @@ class Sabores(Base):
         self.nome_sabor = nome_sabor
         self.descricao_sabor = descricao_sabor
         self.alerta_alergenicos = alerta_alergenicos
-        
+
 # ItensPedido
 class ItensPedido(Base):
-    __table_name__ = 'ItensPedido'
+    __table_name__ = 'itenspedido'
 
     TAMANHO_ITENS = (
         ("INDIVIDUAL", "INDIVIDUAL"),
@@ -89,7 +68,7 @@ class ItensPedido(Base):
 
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     quantidade = Column("quantidade", Integer, nullable=False, default=0)
-    sabor = Column("sabor", ForeignKey("sabor.id"), nullable=False)
+    sabor = Column("sabor", ForeignKey("sabores.id"), nullable=False)
     tamanho = Column("tamanho", ChoiceType(choices=TAMANHO_ITENS), nullable=False)
     tipo_borda = Column("tipo_borda", ChoiceType(choices=BORDA_ITENS), default="VAZIO")
     preco_unitario = Column("preco_unitario", Float, nullable=False)
@@ -103,6 +82,27 @@ class ItensPedido(Base):
         self.id_pedido = id_pedido
         self.tipo_borda = tipo_borda
 
+# Pedido
+class Pedido(Base):
+    __table_name__ = 'pedidos'
+
+    # Escolha de somente três informações para o campo "status"
+    STATUS_PEDIDOS = (
+        ("PENDENTE", "PENDENTE"),
+        ("FINALIZADO", "FINALIZADO"),
+        ("CANCELADO", "CANCELADO")
+    )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", ChoiceType(choices=STATUS_PEDIDOS), nullable=False, default="PENDENTE")
+    id_usuario = Column("id_usuario", ForeignKey("usuarios.id"), nullable=False)
+    preco = Column("preco", Float, nullable=False)
+    itens = Column("itens", ForeignKey("itenspedido.id"))
+
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.status = status
+        self.preco = preco
 
 # TODO: Criar metadados do database (criar efetivamente o database)
 
